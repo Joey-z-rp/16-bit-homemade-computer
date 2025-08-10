ed815b9b6d03d678179447bda7e5f54adcd87985
Joey Zheng <joey.zheng@smokeball.com>
2025-08-10 14:33:48 +1000

    add prompt

--- toolchain/example-programs/game-and-music.asm
+++ toolchain/example-programs/game-and-music.asm
@@ -1,11 +1,19 @@
 // The number of loops to delay
 @delay_loop_number
 M=0
-// The address to jump to after the delay, initialized to MAIN
-@MAIN
-D=A
+// The address to jump to after the delay
 @delay_return_address
-M=D
+M=0
+
+// The string array address
+@print_string_address
+M=0
+// The number of characters in the string
+@print_string_length
+M=0
+// Print string return address
+@print_string_return_address
+M=0
 
 // The last key pressed
 @last_key
@@ -20,22 +28,157 @@ M=0
 @is_mode_prompt_printed
 M=0
 
-(DELAY)
-@delay_counter
-M=0
-(DELAY_LOOP)
-@delay_counter
-D=M
-@delay_loop_number // delay 0.02ms per loop at 500kHz
-D=D-M
-@delay_return_address
-A=M
-D;JGT
-@delay_counter
-M=M+1
-@DELAY_LOOP
-0;JMP
+// Select action prompt stored at 300: "SELECT ACTION"
+@300
+D=A
+@select_action_prompt_address
+M=D
+// The number of characters in the select action prompt
+@14
+D=A
+@select_action_prompt_length
+M=D
+// "S"
+@83
+D=A
+@300
+M=D
+// "E"
+@69
+D=A
+@301
+M=D
+// "L"
+@76
+D=A
+@302
+M=D
+// "E"
+@69
+D=A
+@303
+M=D
+// "C"
+@67
+D=A
+@304
+M=D
+// "T"
+@84
+D=A
+@305
+M=D
+// " "
+@32
+D=A
+@306
+M=D
+// "A"
+@65
+D=A
+@307
+M=D
+// "C"
+@67
+D=A
+@308
+M=D
+// "T"
+@84
+D=A
+@309
+M=D
+// "I"
+@73
+D=A
+@310
+M=D
+// "O"
+@79
+D=A
+@311
+M=D
+// "N"
+@78
+D=A
+@312
+M=D
+// ":"
+@58
+D=A
+@313
+M=D
 
+// Music prompt stored at 314: "PRESS NUMBER"
+@314
+D=A
+@music_prompt_address
+M=D
+// The number of characters in the music prompt
+@12
+D=A
+@music_prompt_length
+M=D
+// "P"
+@80
+D=A
+@314
+M=D
+// "R"
+@82
+D=A
+@315
+M=D
+// "E"
+@69
+D=A
+@316
+M=D
+// "S"
+@83
+D=A
+@317
+M=D
+// "S"
+@83
+D=A
+@318
+M=D
+// " "
+@32
+D=A
+@319
+M=D
+// "N"
+@78
+D=A
+@320
+M=D
+// "U"
+@85
+D=A
+@321
+M=D
+// "M"
+@77
+D=A
+@322
+M=D
+// "B"
+@66
+D=A
+@323
+M=D
+// "E"
+@69
+D=A
+@324
+M=D
+// "R"
+@82
+D=A
+@325
+M=D
 
 (MAIN)
 
@@ -63,31 +206,36 @@ D=M
 D;JGT
 
 // Select action prompt
-@83 // S
-D=A
-@UI_CMD_1
+@select_action_prompt_address
+D=M
+@print_string_address
 M=D
-@WAIT_MUSIC_PROMPT_EXECUTED
+@select_action_prompt_length
+D=M
+@print_string_length
+M=D
+@SET_MODE_PROMPT_PRINTED
+D=A
+@print_string_return_address
+M=D
+@PRINT_STRING
 0;JMP
 
 // Music prompt
 (MUSIC_PROMPT)
-@77 // M
-D=A
-@UI_CMD_1
+@music_prompt_address
+D=M
+@print_string_address
 M=D
-
-// delay for the command to be executed
-(WAIT_MUSIC_PROMPT_EXECUTED)
-@50
-D=A
-@delay_loop_number
+@music_prompt_length
+D=M
+@print_string_length
 M=D
 @SET_MODE_PROMPT_PRINTED
 D=A
-@delay_return_address
+@print_string_return_address
 M=D
-@DELAY
+@PRINT_STRING
 0;JMP
 
 (SET_MODE_PROMPT_PRINTED)
@@ -122,4 +270,56 @@ D;JEQ
 (MUSIC)
 // TODO: Implement the music logic
 @MAIN
+0;JMP
+
+(DELAY)
+@delay_counter
+M=0
+(DELAY_LOOP)
+@delay_counter
+D=M
+@delay_loop_number // delay 0.02ms per loop at 500kHz
+D=D-M
+@delay_return_address
+A=M
+D;JGT
+@delay_counter
+M=M+1
+@DELAY_LOOP
+0;JMP
+
+(PRINT_STRING)
+// Load the current character address
+@print_string_address
+A=M
+D=M
+// Send the character to UI_CMD_1
+@UI_CMD_1
+M=D
+// Add delay after sending command
+@100
+D=A
+@delay_loop_number
+M=D
+@PRINT_STRING_CONTINUE
+D=A
+@delay_return_address
+M=D
+@DELAY
+0;JMP
+
+(PRINT_STRING_CONTINUE)
+// Increment the string address for next character
+@print_string_address
+M=M+1
+// Decrement the remaining length
+@print_string_length
+M=M-1
+D=M
+// If length > 0, continue printing
+@PRINT_STRING
+D;JGT
+// If length = 0, we're done - return
+@print_string_return_address
+A=M
 0;JMP
